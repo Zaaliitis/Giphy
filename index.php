f1051dfd4bf36b8673a7f8b1105340c83d5deb98
Zalitis <zrobertss@gmail.com>
2023-04-25 17:58:41 +0300

    Added logo

--- index.php
+++ index.php
@@ -5,17 +5,24 @@
 </head>
 <body>
 <div style="text-align:center;">
-<form action="/" method="GET">
-    <label for="search_query">Search for GIFs:</label>
-    <input type="text" id="search_query" name="search_query">
-    <button type="submit">Search</button>
-</form>
+<img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Giphy-logo.svg/1280px-Giphy-logo.svg.png"
+    width="500"
+    alt="Giphy logo"
+>
+<br><br>
+    <form action="/" method="GET">
+        <label for="search_query">Search for GIFs:</label>
+        <input type="text" id="search_query" name="search_query">
+        <button type="submit">Search</button>
+    </form>
 </div>
 
 <?php
 require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
 use App\ApiClient;
 
 $client = new ApiClient();
@@ -24,9 +31,10 @@ $client->getGifs($searchQuery);
 ?>
 <div style="text-align:center;">
     <br>
-    <button style="font-size: 40px; padding: 20px 40px; background-color: #4E5B31; color: black; border: 2px solid black; border-radius: 5px;"
-            type="button"
-            onclick="location.href='/index.php?search_query=<?= $searchQuery ?>&offset=<?= $client->getOffset() ?>'">
+    <button
+        style="font-size: 40px; padding: 20px 40px; background-color: #4E5B31; color: black; border: 2px solid black; border-radius: 5px;"
+        type="button"
+        onclick="location.href='/index.php?search_query=<?= $searchQuery ?>&offset=<?= $client->getOffset() ?>'">
         MORE GIFs
     </button>
 </div>
